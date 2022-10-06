c4aebc109aad3c2c10f10bf75a0d4a8354787c6d
Legacy107 <quocmai1107@gmail.com>
2022-10-06 17:54:09 +1100

    feat: add GameManager

---
+++ BattleOfTanks/GameManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SplashKitSDK;
+
+namespace BattleOfTanks
+{
+    public class GameManager
+    {
+        private Window _window;
+        private const string _clock = "game clock";
+        private double _prevTime; // for calculating delta time
+        private Tank _playerTank;
+        private List<Tank> _enemyTanks;
+
+        public GameManager(Window window)
+        {
+            _window = window;
+
+            SplashKit.CreateTimer(_clock);
+            SplashKit.StartTimer(_clock);
+            _prevTime = 0;
+
+            _playerTank = new Tank(100, 100);
+            _enemyTanks = new List<Tank>();
+        }
+
+        // return true on exit command
+        public bool HandleInput()
+        {
+            SplashKit.ProcessEvents();
+
+            if (SplashKit.KeyTyped(KeyCode.EscapeKey) || SplashKit.QuitRequested())
+                return true;
+
+            const double moveForce = 7000;
+            if (SplashKit.KeyDown(KeyCode.DownKey))
+                _playerTank.ApplyForce(SplashKit.VectorTo(0, moveForce));
+            if (SplashKit.KeyDown(KeyCode.UpKey))
+                _playerTank.ApplyForce(SplashKit.VectorTo(0, -moveForce));
+            if (SplashKit.KeyDown(KeyCode.LeftKey))
+                _playerTank.ApplyForce(SplashKit.VectorTo(-moveForce, 0));
+            if (SplashKit.KeyDown(KeyCode.RightKey))
+                _playerTank.ApplyForce(SplashKit.VectorTo(moveForce, 0));
+
+            if (SplashKit.KeyDown(KeyCode.WKey))
+                _playerTank.MoveForward(moveForce);
+            if (SplashKit.KeyDown(KeyCode.SKey))
+                _playerTank.MoveBackward(moveForce);
+
+            _playerTank.RotateToPoint(SplashKit.MousePosition());
+
+            return false;
+        }
+
+        public void Update()
+        {
+            int curTime = (int)SplashKit.TimerTicks(_clock);
+            double delta = (curTime - _prevTime) / 1000.0; // milisecond to second
+            _prevTime = curTime;
+
+            _playerTank.Update(delta);
+        }
+
+        public void Draw()
+        {
+            SplashKit.ClearScreen(Color.White);
+            _playerTank.Draw(_window);
+            SplashKit.DrawText("Speed: " + SplashKit.VectorMagnitude(_playerTank.Velo), Color.Black, 10, 10);
+
+            SplashKit.RefreshScreen(60);
+        }
+
+        public void ChangeLevel()
+        {
+        }
+
+        public void MainLoop()
+        {
+            while (true)
+            {
+                if (HandleInput())
+                {
+                    CleanUp();
+                    break;
+                }
+                Update();
+                Draw();
+            }
+        }
+
+        public void CleanUp()
+        {
+        }
+    }
+}
+
--- BattleOfTanks/Program.cs
+++ BattleOfTanks/Program.cs
@@ -13,59 +13,10 @@ namespace BattleOfTanks
         public static void Main()
         {
             LoadResources();
-            Window window = new Window("Custom Program", 800, 600);
-            Tank tank = new Tank(100, 100);
-            Timer timer = new Timer("clock");
-            int prevTime = 0;
-            timer.Start();
-            while(true)
-            {
-                int curTime = (int)timer.Ticks;
-                double delta = (curTime - prevTime) / 1000.0;
-                prevTime = curTime;
+            Window window = new Window("Custom Program", 1000, 800);
+            GameManager gameManager = new GameManager(window);
 
-                SplashKit.ProcessEvents();
-
-                if (SplashKit.KeyTyped(KeyCode.EscapeKey) || SplashKit.QuitRequested())
-                {
-                    break;
-                }
-
-                const double moveForce = 7000;
-                if (SplashKit.KeyDown(KeyCode.DownKey))
-                {
-                    tank.ApplyForce(SplashKit.VectorTo(0, moveForce));
-                }
-                if (SplashKit.KeyDown(KeyCode.UpKey))
-                {
-                    tank.ApplyForce(SplashKit.VectorTo(0, -moveForce));
-                }
-                if (SplashKit.KeyDown(KeyCode.LeftKey))
-                {
-                    tank.ApplyForce(SplashKit.VectorTo(-moveForce, 0));
-                }
-                if (SplashKit.KeyDown(KeyCode.RightKey))
-                {
-                    tank.ApplyForce(SplashKit.VectorTo(moveForce, 0));
-                }
-                if (SplashKit.KeyDown(KeyCode.WKey))
-                {
-                    tank.MoveForward(moveForce);
-                }
-                if (SplashKit.KeyDown(KeyCode.SKey))
-                {
-                    tank.MoveBackward(moveForce);
-                }
-                tank.RotateToPoint(SplashKit.MousePosition());
-
-                tank.Update(delta);
-
-                SplashKit.ClearScreen(Color.White);
-                tank.Draw(window);
-                SplashKit.DrawText("Speed: " + SplashKit.VectorMagnitude(tank.Velo), Color.Black, 10, 10);
-
-                SplashKit.RefreshScreen(60);
-            }
+            gameManager.MainLoop();
         }
     }
 }
